4a7c9ac8c6d4b1e9a90c560983be425ad9de43e7
danrcurry <45722842+danrcurry@users.noreply.github.com>
2018-12-08 20:56:31 -0800

    Update ManagerView.java
    Updated ManagerView

--- ManagerView.java
+++ ManagerView.java
@@ -9,230 +9,329 @@ import java.util.ArrayList;
 import java.io.IOException;
 
 /**
- * @author Brogrammers
- * 
- * View class for the manager portion of the project. Creates and provides a clickable calendar for viewing reserved rooms and 
- * a rooms view that allows manager to view reservations by room number.
- *
+ * ManagerView
+ * @author TeamVoid
+ * View meant for manager user accounts to view additional information and have additional functionality
  */
-public class ManagerView extends JFrame {
+public class ManagerView extends JFrame 
+{
 	private final int WIDTH = 925;
 	private final int HEIGHT = 575;
 	private final int TEXT_AREA_WIDTH = 25;
 	private final int TEXT_AREA_HEIGHT = 40;
 	private final int CALENDAR_CELL_HEIGHT = 50;
-	private final String[] MONTHS = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	private final String[] MONTHS = { 
+			"January","February","March","April","May","June","July","August","September","October","November","December"};
 	private final String[] DAYS = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 	private final int DAYS_PER_WEEK = 7;
 	private final int WEEKS_PER_MONTH = 6;
 
-	private ReservationSystem model;
+	private ReservationSystem _reservationSystem;
 	
-	private JTabbedPane managerTabs;
-	private JLabel monthAndYearLabel, roomNumbersLabel;
-	private JButton prevMonthButton, nextMonthButton, prevYearButton, nextYearButton, saveButton, loadButton;
-	private JScrollPane calendarScrollPane, roomsScrollPane;
-	private JPanel calendarPanel, calendarButtonPanel, calendarInfoPanel, roomsPanel, roomsInfoPanel;
-	private DefaultTableModel calendarModel;
-	private JTable calendarTable;
-	private JList<Integer> roomsList;
-	private JTextArea calendarInfoArea, roomsInfoArea;
+	private JLabel monthYearJLabel;
+	private JLabel roomNumbersJLabel;
 	
-	int selectedCalendarRow, selectedCalendarColumn;
+	private JTextArea calendarInformationJTextArea;
+	private JTextArea roomsInformationJTextArea;
+	
+	private JButton previousMonthJButton; 
+	private JButton nextMonthJButton;
+	private JButton previousYearJButton; 
+	private JButton nextYearJButton; 
+	private JButton saveJButton;
+	private JButton loadJButton;
+	
+	private JScrollPane calendarJScrollPane; 
+	private JScrollPane roomsJScrollPane;
+	
+	private JPanel calendarJPanel;
+	private JPanel calendarButtonsJPanel; 
+	private JPanel calendarInformationJPanel; 
+	private JPanel roomsJPanel;
+	private JPanel roomsInformationJPanel;
+	
+	private DefaultTableModel calendarTableModel;
+	private JTable calendarJTable;
+	private JList<Integer> roomsJList;
+	private JTabbedPane managerTabbedPane;
 
-	public ManagerView(ReservationSystem model) {
-		this.model = model;
+	
+	int selectedCalendarRow;
+	int selectedCalendarColumn;
+
+	/**
+	 * ManagerView
+	 * @param reservationSystem
+	 * View for the manager, that contains views and functionality that only the manager has access to
+	 */
+	public ManagerView(ReservationSystem reservationSystem) 
+	{
+		this._reservationSystem = reservationSystem;
 		
-		ChangeListener cl = new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
+		ChangeListener changeListener = new ChangeListener() 
+		{
+			public void stateChanged(ChangeEvent event) 
+			{
 				updateTableModel();
-				highlightSelectedCell();
-				updateLabels();
+				selectCell();
+				updateCalendarLabels();
 				updateCalendar();
-				updateRoomsView();
+				updateRoomsInformationJTextArea();
 			}
 		};
-		model.addListener(cl);
+		
+		_reservationSystem.addListener(changeListener);
 		
 		setTitle("Manager View");
 		setLayout(new FlowLayout());
 		setSize(WIDTH, HEIGHT);
 		setResizable(false);
 		
-		managerTabs = new JTabbedPane();
-	
-		calendarPanel = new JPanel(new BorderLayout()); // adds components of calendar panel
+		managerTabbedPane = new JTabbedPane();
+		calendarJPanel = new JPanel(new BorderLayout());
+		monthYearJLabel = new JLabel();
 		
-		monthAndYearLabel = new JLabel();
-		updateLabels();
+		updateCalendarLabels();
 		
-		buildCalendarTableModel();
-		buildCalendarPanel();
+		createCalendarTableModel();
+		createCalendarJPanel();
 		
-		calendarInfoPanel = new JPanel(new BorderLayout());
+		calendarInformationJPanel = new JPanel(new BorderLayout());
 		
-		calendarInfoArea = new JTextArea(TEXT_AREA_WIDTH, TEXT_AREA_HEIGHT);
-		saveButton = new JButton("Save all"); // SAVES MODEL'S DATA USING SERIALIZATION
-		saveButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				model.save();
-				JOptionPane.showMessageDialog(ManagerView.this, "Save successful", "Reservations saved", JOptionPane.PLAIN_MESSAGE);
+		calendarInformationJTextArea = new JTextArea(TEXT_AREA_WIDTH, TEXT_AREA_HEIGHT);
+		saveJButton = new JButton("Save everything!");
+		saveJButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent actionEvent) 
+			{
+				_reservationSystem.save();
+				JOptionPane.showMessageDialog(ManagerView.this, "Your save was successful", "The reservations were saved", JOptionPane.PLAIN_MESSAGE);
 			}
 		});
-		loadButton = new JButton("Load");
-		loadButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		
+		loadJButton = new JButton("Load Information");
+		loadJButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent actionEvent) 
+			{
 				try {
-					model.load();
-					JOptionPane.showMessageDialog(ManagerView.this, "Loading successful", "Reservations loaded", JOptionPane.PLAIN_MESSAGE);
+					_reservationSystem.load();
+					JOptionPane.showMessageDialog(ManagerView.this, "Loading the reservations was successful", "The reservations were loaded", JOptionPane.PLAIN_MESSAGE);
 				}
-				catch(ClassNotFoundException i) {
-					JOptionPane.showMessageDialog(ManagerView.this, "Loading failed", "File not found", JOptionPane.ERROR_MESSAGE);
+				catch(ClassNotFoundException notFoundException) 
+				{
+					JOptionPane.showMessageDialog(ManagerView.this, "Loading the reserversations was not successful", "The file was not found", JOptionPane.ERROR_MESSAGE);
 				}
-				catch(Exception ex) {
-					JOptionPane.showMessageDialog(ManagerView.this, "Loading failed", "There was an error", JOptionPane.ERROR_MESSAGE);
+				catch(Exception ex) 
+				{
+					JOptionPane.showMessageDialog(ManagerView.this, "Loading the reservations was not successful", "There was an unknown error", JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
-		calendarInfoPanel.add(loadButton, BorderLayout.NORTH);
-		calendarInfoPanel.add(calendarInfoArea, BorderLayout.CENTER);
-		calendarInfoPanel.add(saveButton, BorderLayout.SOUTH);
-		calendarPanel.add(calendarInfoPanel, BorderLayout.EAST);
 		
-		roomsPanel = new JPanel(new BorderLayout()); // adds components of rooms panel
+		calendarInformationJPanel.add(loadJButton, BorderLayout.NORTH);
+		calendarInformationJPanel.add(calendarInformationJTextArea, BorderLayout.CENTER);
+		calendarInformationJPanel.add(saveJButton, BorderLayout.SOUTH);
+		calendarJPanel.add(calendarInformationJPanel, BorderLayout.EAST);
+		
+		roomsJPanel = new JPanel(new BorderLayout());
 
-		buildRoomsListPanel();
+		createRoomsListPanel();
 
-		roomsInfoPanel = new JPanel(new BorderLayout());
+		roomsInformationJPanel = new JPanel(new BorderLayout());
 		
-		roomsInfoArea = new JTextArea(TEXT_AREA_WIDTH, TEXT_AREA_HEIGHT);
-		roomsInfoPanel.add(roomsInfoArea, BorderLayout.CENTER);
-		roomsPanel.add(roomsInfoPanel);
+		roomsInformationJTextArea = new JTextArea(TEXT_AREA_WIDTH, TEXT_AREA_HEIGHT);
 		
-		managerTabs.addTab("Calendar", calendarPanel);
-		managerTabs.addTab("Rooms", roomsPanel);
-		add(managerTabs);
+		roomsInformationJPanel.add(roomsInformationJTextArea, BorderLayout.CENTER);
+		roomsJPanel.add(roomsInformationJPanel);
+		
+		managerTabbedPane.addTab("Calendar", calendarJPanel);
+		managerTabbedPane.addTab("Rooms", roomsJPanel);
+		
+		add(managerTabbedPane);
 		
 		setLocationRelativeTo(null);
 		//setVisible(true);
 	}
 	
-	private void buildRoomsListPanel() {
-		roomNumbersLabel = new JLabel("Room information:");
+	/**
+	 * createRoomsListPanel
+	 * 
+	 * Method that creates the list panel for the rooms
+	 */
+	private void createRoomsListPanel() 
+	{
+		roomNumbersJLabel = new JLabel("Room Information by Room Number:");
 		
-		ListSelectionListener l = new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent e) {
-				updateRoomsView();
+		ListSelectionListener listSelectionListener = new ListSelectionListener() 
+		{
+			public void valueChanged(ListSelectionEvent listSelectionEvent) 
+			{
+				updateRoomsInformationJTextArea();
 			}
 		};
 		
-		Integer[] rooms = new Integer[ReservationSystem.NUMBER_OF_ROOMS];
-		for(int i = 0; i < rooms.length; i++) {
-			rooms[i] = i + 1;
+		Integer[] numberOfRooms = new Integer[ReservationSystem.NUMBER_OF_ROOMS];
+		
+		for(int i = 0; i < numberOfRooms.length; i++) 
+		{
+			numberOfRooms[i] = i + 1;
 		}
 		
-		roomsList = new JList(rooms);
-		roomsList.addListSelectionListener(l);
+		JList roomsJList = new JList(numberOfRooms);
 		
-		roomsScrollPane = new JScrollPane(roomsList);
+		roomsJList.addListSelectionListener(listSelectionListener);
 		
-		roomsPanel.add(roomNumbersLabel, BorderLayout.NORTH);
-		roomsPanel.add(roomsScrollPane, BorderLayout.WEST);
+		roomsJScrollPane = new JScrollPane(roomsJList);
+		
+		roomsJPanel.add(roomNumbersJLabel, BorderLayout.NORTH);
+		roomsJPanel.add(roomsJScrollPane, BorderLayout.WEST);
 	}
 	
-	private void updateRoomsView() {
-		ArrayList<Reservation> reservations = model.getReservationsByRoomNumber(roomsList.getSelectedIndex());
+	/**
+	 * updateRoomsInformationJTextArea
+	 * 
+	 * Method that updates the room information text area with new information
+	 */
+	private void updateRoomsInformationJTextArea() 
+	{
+		ArrayList<Reservation> reservations = _reservationSystem.getReservationsByRoomNumber(roomsJList.getSelectedIndex());
 		
-		String list = "";
-		for(Reservation r : reservations) {
-			list += r.toString() + "\n";
+		String reservationList = "";
+		for(Reservation reservation : reservations) 
+		{
+			reservationList = reservationList + reservation.toString() + "\n";
 		}
-		
-		roomsInfoArea.setText(list);
+		roomsInformationJTextArea.setText(reservationList);
 	}
 	
-	private void buildCalendarPanel() {
-		prevMonthButton = new JButton("<<");
-		nextMonthButton = new JButton(">>");
-		prevYearButton = new JButton("<");
-		nextYearButton = new JButton(">");
+	/**
+	 * createCalendarJPanel
+	 * 
+	 * Method that creates the panel containing the calendar information
+	 */
+	private void createCalendarJPanel() 
+	{
+		previousMonthJButton = new JButton("--");
+		nextMonthJButton = new JButton("++");
+		previousYearJButton = new JButton("-");
+		nextYearJButton = new JButton("+");
 		
-		prevMonthButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				model.previousMonth();
-			}
-		});
-		nextMonthButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				model.nextMonth();
-			}
-		});
-		prevYearButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				model.previousYear();
-			}
-		});
-		nextYearButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				model.nextYear();
+		previousMonthJButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				_reservationSystem.previousMonth();
 			}
 		});
 		
-		calendarPanel.add(monthAndYearLabel, BorderLayout.NORTH);
+		nextMonthJButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent actionEvent) 
+			{
+				_reservationSystem.nextMonth();
+			}
+		});
 		
-		calendarScrollPane = new JScrollPane(calendarTable);
-		calendarPanel.add(calendarScrollPane, BorderLayout.CENTER);
+		previousYearJButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent actionEvent) 
+			{
+				_reservationSystem.previousYear();
+			}
+		});
 		
-		calendarButtonPanel = new JPanel();
-		calendarButtonPanel.add(prevYearButton);
-		calendarButtonPanel.add(prevMonthButton);
-		calendarButtonPanel.add(nextMonthButton);
-		calendarButtonPanel.add(nextYearButton);
-		calendarPanel.add(calendarButtonPanel, BorderLayout.SOUTH);
+		nextYearJButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent actionEvent) 
+			{
+				_reservationSystem.nextYear();
+			}
+		});
+		
+		calendarJScrollPane = new JScrollPane(calendarJTable);
+		
+		calendarButtonsJPanel = new JPanel();
+		calendarButtonsJPanel.add(previousYearJButton);
+		calendarButtonsJPanel.add(previousMonthJButton);
+		calendarButtonsJPanel.add(nextMonthJButton);
+		calendarButtonsJPanel.add(nextYearJButton);
+		
+		calendarJPanel.add(monthYearJLabel, BorderLayout.NORTH);
+		calendarJPanel.add(calendarJScrollPane, BorderLayout.CENTER);
+		calendarJPanel.add(calendarButtonsJPanel, BorderLayout.SOUTH);
 	}
 	
-	private void buildCalendarTableModel() {
-		MouseAdapter m = new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				selectedCalendarRow = calendarTable.getSelectedRow();
-				selectedCalendarColumn = calendarTable.getSelectedColumn();
+	/**
+	 * createCalendarTableModel
+	 * 
+	 * Method that creates the calendar model to be used within the calendar panels
+	 */
+	private void createCalendarTableModel() 
+	{
+		MouseAdapter mouseAdapter = new MouseAdapter() 
+		{
+			public void mousePressed(MouseEvent mouseEvent) 
+			{
+				selectedCalendarRow = calendarJTable.getSelectedRow();
+				selectedCalendarColumn = calendarJTable.getSelectedColumn();
 				
-				int day = (int) calendarTable.getValueAt(selectedCalendarRow, selectedCalendarColumn);
-				model.goToDate(model.getCalendar().get(Calendar.MONTH), day, model.getCalendar().get(Calendar.YEAR));
+				int day = (int) calendarJTable.getValueAt(selectedCalendarRow, selectedCalendarColumn);
+				_reservationSystem.goToDate(_reservationSystem.getCalendar().get(Calendar.MONTH), day, _reservationSystem.getCalendar().get(Calendar.YEAR));
 				
 				updateCalendar();
 			}
 		};
 		
-		calendarModel = new DefaultTableModel(buildMonthArray(), DAYS);
-		calendarTable = new JTable(calendarModel) {
-			public boolean isCellEditable(int row, int col) {
+		calendarTableModel = new DefaultTableModel(createMonthArray(), DAYS);
+		
+		calendarJTable = new JTable(calendarTableModel) 
+		{
+			public boolean isCellEditable(int row, int col) 
+			{
 				return false;
 			}
 		};
-		calendarTable.setRowHeight(CALENDAR_CELL_HEIGHT);
-		calendarTable.setCellSelectionEnabled(true);
-		calendarTable.addMouseListener(m);
-		highlightSelectedCell();
+		
+		calendarJTable.setRowHeight(CALENDAR_CELL_HEIGHT);
+		calendarJTable.setCellSelectionEnabled(true);
+		calendarJTable.addMouseListener(mouseAdapter);
+		
+		selectCell();
 	}
 	
-	private Integer[][] buildMonthArray() {
+	/**
+	 * createMonthArray
+	 * @return
+	 * Method that returns the month and day 
+	 */
+	private Integer[][] createMonthArray() 
+	{
 		Integer[][] temp = new Integer[WEEKS_PER_MONTH][DAYS_PER_WEEK];
 		
-		int month = model.getCalendar().get(Calendar.MONTH);
-		int year = model.getCalendar().get(Calendar.YEAR);
+		int month = _reservationSystem.getCalendar().get(Calendar.MONTH);
+		int year = _reservationSystem.getCalendar().get(Calendar.YEAR);
+		
 		Calendar tempCal = Calendar.getInstance();
+		
 		tempCal.set(year, month, 1);
 		
 		int firstDay = tempCal.get(Calendar.DAY_OF_WEEK) - 1;
+		
 		int day = 1;
 		
-		for(int i = 0; i < WEEKS_PER_MONTH; i++) {
-			for(int j = 0; j < DAYS_PER_WEEK; j++) {
+		for(int i = 0; i < WEEKS_PER_MONTH; i++) 
+		{
+			for(int j = 0; j < DAYS_PER_WEEK; j++) 
+			{
 				if((i != 0 || j >= firstDay) && (day <= tempCal.getActualMaximum(Calendar.DAY_OF_MONTH)))
+				{
 					temp[i][j] = day++;
+				}
 					
-				if(day == model.getCalendar().get(Calendar.DAY_OF_MONTH) + 1) {
+					
+				if(day == _reservationSystem.getCalendar().get(Calendar.DAY_OF_MONTH) + 1) 
+				{
 					selectedCalendarRow = i;
 					selectedCalendarColumn = j;
 				}
@@ -241,36 +340,64 @@ public class ManagerView extends JFrame {
 		return temp;
 	}
 	
-	private void updateCalendar() {
-		Date currentDate = model.getCalendar().getTime();
-		ArrayList<Reservation> reservations = model.getReservationsByDate(currentDate);
+	/**
+	 * updateCalendar
+	 * 
+	 * Method that updates the calendar based on the current date
+	 */
+	private void updateCalendar() 
+	{
+		Date currentDate = _reservationSystem.getCalendar().getTime();
+		ArrayList<Reservation> reservations = _reservationSystem.getReservationsByDate(currentDate);
 		
-		String list = "";
-		for(Reservation r : reservations) {
-			list += r.toString() + "\n";
+		String reserationList = "";
+		for(Reservation reservation : reservations) 
+		{
+			reserationList = reserationList + reservation.toString() + "\n";
 		}
-		
-		calendarInfoArea.setText(list);
+		calendarInformationJTextArea.setText(reserationList);
 	}
 	
-	private void updateLabels() {
-		monthAndYearLabel.setText(MONTHS[model.getCalendar().get(Calendar.MONTH)] + " " + model.getCalendar().get(Calendar.YEAR));
+	/**
+	 * updateCalendarLabels
+	 * 
+	 * Method that updates the labels used for the calendar
+	 */
+	private void updateCalendarLabels() 
+	{
+		monthYearJLabel.setText(MONTHS[_reservationSystem.getCalendar().get(Calendar.MONTH)] + " " + _reservationSystem.getCalendar().get(Calendar.YEAR));
 	}
 	
-	private void updateTableModel() {
-		Integer[][] temp = buildMonthArray();
+	/**
+	 * updateTableModel
+	 * 
+	 * Method that updates the calendar table model with newly created calendar table model 
+	 */
+	private void updateTableModel() 
+	{
+		Integer[][] temp = createMonthArray();
 	
-		calendarModel = new DefaultTableModel(temp, DAYS);
-		calendarTable.setModel(calendarModel);
+		calendarTableModel = new DefaultTableModel(temp, DAYS);
+		calendarJTable.setModel(calendarTableModel);
 	}
 	
-	private void highlightSelectedCell() {
-		for(int i = 0; i < calendarModel.getRowCount(); i++) {
-			for(int j = 0; j < calendarModel.getColumnCount(); j++) {
-				Integer val = (Integer) calendarModel.getValueAt(i, j);
+	/**
+	 * selectCell
+	 * 
+	 * Method that shows the selected cell.
+	 */
+	private void selectCell() 
+	{
+		for(int i = 0; i < calendarTableModel.getRowCount(); i++) 
+		{
+			for(int j = 0; j < calendarTableModel.getColumnCount(); j++) 
+			{
+				Integer val = (Integer) calendarTableModel.getValueAt(i, j);
 				
-				if(val != null && val == model.getCalendar().get(Calendar.DAY_OF_MONTH))
-					calendarTable.changeSelection(i, j, false, false);
+				if(val != null && val == _reservationSystem.getCalendar().get(Calendar.DAY_OF_MONTH))
+				{
+					calendarJTable.changeSelection(i, j, false, false);
+				}
 			}
 		}
 	}
